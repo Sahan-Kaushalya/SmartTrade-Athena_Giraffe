3eea47e5b3b68c546908732bec0b4315f94861df
Anjana Dilhara <122281334+CCapitalTech@users.noreply.github.com>
2025-12-15 16:21:04 +0530

    Add new LINEs -  DEFAULT_SELECTOR_VALUE, MAIN_APP_CURRENCY,  APP_COUNTRY

--- src/main/java/lk/jiat/smarttrade/util/AppUtil.java
+++ src/main/java/lk/jiat/smarttrade/util/AppUtil.java
@@ -6,6 +6,9 @@ import java.security.SecureRandom;
 
 public class AppUtil {
     public static final Gson GSON = new Gson();
+    public static final int DEFAULT_SELECTOR_VALUE = 0;
+    public static final String MAIN_APP_CURRENCY = "LKR";
+    public static final String APP_COUNTRY = "Sri Lanka";
     private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     public static String generateCode() {
