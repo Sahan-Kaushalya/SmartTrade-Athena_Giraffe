72fdf18745509f9dc8c9b61db838354b5c657775
Anjana Dilhara <122281334+CCapitalTech@users.noreply.github.com>
2025-12-17 12:39:02 +0530

    Make return url and call order complete and fail methods

--- src/main/java/lk/jiat/smarttrade/controller/api/PaymentGatewayController.java
+++ src/main/java/lk/jiat/smarttrade/controller/api/PaymentGatewayController.java
@@ -1,16 +1,23 @@
 package lk.jiat.smarttrade.controller.api;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
+import lk.jiat.smarttrade.service.OrderService;
+import lk.jiat.smarttrade.util.Env;
+import lk.jiat.smarttrade.util.PayHereUtil;
+
+import java.net.URI;
 
 @Path("/payments")
 public class PaymentGatewayController {
     @Path("/return")
     @GET
-    public Response paymentSuccess() {
-        System.out.println("return");
-        return Response.ok().entity("{name:'Anjana'}").build();
+    public Response paymentSuccess(@QueryParam("orderId)") String orderId) {
+        return Response.seeOther(
+                URI.create(Env.get("app.url") + "/invoice.html?orderId=" + orderId)
+        ).build();
     }
 
     @Path("/cancel")
@@ -20,10 +27,24 @@ public class PaymentGatewayController {
         return Response.ok().build();
     }
 
+    @POST
     @Path("/notify")
-    @GET
-    public Response paymentNotify() {
-        System.out.println("notify");
-        return Response.ok().build();
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response notify(MultivaluedMap<String, String> form) {
+
+        String orderId = form.getFirst("order_id");
+        String statusCode = form.getFirst("status_code");
+
+        if (!PayHereUtil.validateNotify(form)) {
+            return Response.status(400).entity("INVALID SIGNATURE").build();
+        }
+        OrderService orderService = new OrderService();
+        if ("2".equals(statusCode)) { // 2 = SUCCESS
+            orderService.completeOrder(orderId);
+        } else {
+            orderService.failOrder(orderId);
+        }
+
+        return Response.ok("OK").build();
     }
 }
